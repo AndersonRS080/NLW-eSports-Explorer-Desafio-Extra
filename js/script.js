1bd6ca3d97d633cb485718324778a4f638c24610
Anderson Rodrigues <105132452+AndersonRS080@users.noreply.github.com>
2022-09-16 17:28:20 -0300

    Atualização

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 ScrollReveal({
   origin: 'right',
   distance: '80px',
-  duration: 1000,
+  duration: 1500,
   
 }).reveal(`section`)
+
+ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 1500,
+  
+}).reveal(`header`)
